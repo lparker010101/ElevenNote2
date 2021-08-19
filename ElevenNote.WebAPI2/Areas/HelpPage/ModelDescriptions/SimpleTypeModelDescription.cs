01a8d9aea37705ae24f55400dd974dcf3a89f66f
Laura Parker <laura.parker03@yahoo.com>
2021-08-18 22:24:10 -0400

    first commit

---
+++ ElevenNote.WebAPI2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ElevenNote.WebAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ElevenNote.WebAPI2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNote.WebAPI2.WebApiApplication" Language="C#" %>
