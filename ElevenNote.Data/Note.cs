b11cfb6b2c38b4861e12df4748532cb4ccbc0de4
Laura Parker <laura.parker03@yahoo.com>
2021-08-26 15:05:58 -0400

    5th committ

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace ElevenNote.Data
             [Required] // Required attribute ensures the user does not enter a null value.
             [Range(1, 5, ErrorMessage ="please choose a number between 1 and 5")]  // The range attribute ensures the user provides a value within the specified range.
             [MaxLength(100, ErrorMessage = "There are too many characters in this field.")] // Attributes such as [MaxLength] limit the amount of characters users can provide.
-            public Guid OwnerId { get; set; }  // Guid is a tyoe that allows us to create unique, near impossible to replicate (like 1 in 1 trillion chance) IDs for our users or items for our users.
+            public Guid OwnerId { get; set; }  // Guid is a type that allows us to create unique, near impossible to replicate (like 1 in 1 trillion chance) IDs for our users or items for our users.
                                                // Guid - Globally Unique IDentifier (32 digit hexadecimals grouped in chunks 8-4-4-12.
                                                // Guid - good for security but bad for debugging b/c they are hard to access.
             [Required]
@@ -28,6 +29,10 @@ namespace ElevenNote.Data
             [Required]
             public DateTimeOffset CreatedUtc { get; set; }
             public DateTimeOffset? ModifiedUtc { get; set; }
+
+            [ForeignKey(nameof(Category))]
+            public Category Category { get; set; }
+
         }
     }
 
